e9de941dc7959b859239c657f7702eb4cc6992d7
Ben Kyrlach <bkyrlach@gmail.com>
2014-06-17 16:45:47 -0400

    Added SafeTryParse

---
+++ FunctionalProgramming/Helpers/Replacements.cs
@@ -0,0 +1,21 @@
+﻿using FunctionalProgramming.Monad;
+
+namespace FunctionalProgramming.Helpers
+{
+    /// <summary>
+    /// Methods that should be part of the stdlib but aren't
+    /// </summary>
+    public static class Replacements
+    {
+        /// <summary>
+        /// Type safe way to parse a string to an Integer
+        /// </summary>
+        /// <param name="s">The string to attempt to parse as an int</param>
+        /// <returns>The parsed int, or nothing if parsing fails (note: not null, but Nothing 'int)</returns>
+        public static IMaybe<int> SafeTryParse(this string s)
+        {
+            int parseResult;
+            return int.TryParse(s, out parseResult) ? parseResult.ToMaybe() : MaybeExtensions.Nothing<int>();
+        }
+    }
+}
