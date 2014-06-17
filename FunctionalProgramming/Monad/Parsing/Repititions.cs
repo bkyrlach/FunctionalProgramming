1415f37133266163d738de4ad4ab3fb875dd91a2
Ben Kyrlach <bkyrlach@gmail.com>
2014-06-17 15:24:31 -0400

    Added missing combinators. Enhanced repetition combinator to be more functional.

---
+++ FunctionalProgramming/Monad/Parsing/Repititions.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalProgramming.Monad.Parsing
+{
+    public abstract class Repititions
+    {
+        public abstract T Match<T>(Func<T> zeroOrMoreReps, Func<T> oneOrMoreReps, Func<int, T> nReps);
+    }
+
+    public sealed class ZeroOrMoreRepititions : Repititions
+    {
+        public static ZeroOrMoreRepititions Only = new ZeroOrMoreRepititions();
+
+        private ZeroOrMoreRepititions()
+        {
+
+        }
+
+        public override T Match<T>(Func<T> zeroOrMoreReps, Func<T> oneOrMoreReps, Func<int, T> nReps)
+        {
+            return zeroOrMoreReps();
+        }
+
+        public override string ToString()
+        {
+            return "ZeroOrMoreRepititions";
+        }
+    }
+
+    public sealed class OneOrMoreRepititions : Repititions
+    {
+        public static OneOrMoreRepititions Only = new OneOrMoreRepititions();
+
+        private OneOrMoreRepititions()
+        {
+
+        }
+
+        public override T Match<T>(Func<T> zeroOrMoreReps, Func<T> oneOrMoreReps, Func<int, T> nReps)
+        {
+            return oneOrMoreReps();
+        }
+
+        public override string ToString()
+        {
+            return "OneOrMoreRepititions";
+        }
+    }
+
+    public sealed class NRepititions : Repititions
+    {
+        private readonly int _n;
+
+        public NRepititions(int n)
+        {
+            _n = n;
+        }
+
+        public override T Match<T>(Func<T> zeroOrMoreReps, Func<T> oneOrMoreReps, Func<int, T> nReps)
+        {
+            return nReps(_n);
+        }
+
+        public override string ToString()
+        {
+            return string.Format("NRepititions({0})", _n);
+        }
+    }
+
+}
