9bcd6016eb07aab79bf3c66b64133c513139c861
Ben Kyrlach <bkyrlach@gmail.com>
2014-06-21 00:56:38 -0400

    Added immutable put for dictionary.

--- FunctionalProgramming/Basics/DictionaryExtensions.cs
+++ FunctionalProgramming/Basics/DictionaryExtensions.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+﻿using System;
 using System.Collections.Generic;
 using FunctionalProgramming.Monad;
 
@@ -6,6 +6,14 @@ namespace FunctionalProgramming.Basics
 {
     public static class DictionaryExtensions    
     {
+        public static IDictionary<TKey, TValue> Put<TKey, TValue>(this IDictionary<TKey, TValue> dictionary,
+            Tuple<TKey, TValue> update)
+        {
+            var newDictionary = new Dictionary<TKey, TValue>(dictionary);
+            newDictionary[update.Item1] = update.Item2;
+            return newDictionary;
+        }
+
         public static IMaybe<TValue> Get<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key)
         {
             return
