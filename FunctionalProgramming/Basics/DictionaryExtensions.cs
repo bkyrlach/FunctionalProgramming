a1f927b424b088ca0f99b063d30d297fd4a062dd
Ben Kyrlach <bkyrlach@gmail.com>
2014-06-21 00:43:43 -0400

    Added safe dictionary access.

---
+++ FunctionalProgramming/Basics/DictionaryExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using FunctionalProgramming.Monad;
+
+namespace FunctionalProgramming.Basics
+{
+    public static class DictionaryExtensions    
+    {
+        public static IMaybe<TValue> Get<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key)
+        {
+            return
+                dictionary.ToMaybe().SelectMany(d => key.ToMaybe().Where(d.ContainsKey).SelectMany(k => d[k].ToMaybe()));
+        } 
+    }
+}
