7ae04b31edba09bb78e0776a1564b0d5ba423533
Ben Kyrlach <bkyrlach@gmail.com>
2014-10-09 16:13:56 -0400

    Fixed monad transformer for Io + Either

--- FunctionalProgramming/Monad/Transformer/IoEither.cs
+++ FunctionalProgramming/Monad/Transformer/IoEither.cs
@@ -2,37 +2,87 @@
 
 namespace FunctionalProgramming.Monad.Transformer
 {
+    public sealed class IoEither<TLeft, TRight>
+    {
+        private readonly Io<IEither<TLeft, TRight>> _self;
+ 
+        public IoEither(Io<IEither<TLeft, TRight>> io)
+        {
+            _self = io;
+        }
+
+        public IoEither(IEither<TLeft, TRight> either) : this(Io<IEither<TLeft, TRight>>.Apply(() => either))
+        {
+            
+        }
+
+        public IoEither(TRight rightVal) : this(rightVal.AsRight<TLeft, TRight>())
+        {
+            
+        }
+
+        public IoEither(TLeft leftVal) : this(leftVal.AsLeft<TLeft, TRight>())
+        {
+            
+        }
+
+        public Io<IEither<TLeft, TRight>> Out()
+        {
+            return _self;
+        }
+
+        public IoEither<TLeft, TResult> FMap<TResult>(Func<TRight, TResult> f)
+        {
+            return new IoEither<TLeft, TResult>(_self.Select(either => either.Select(f)));
+        }
+
+        public IoEither<TLeft, TResult> Bind<TResult>(Func<TRight, IoEither<TLeft, TResult>> f)
+        {
+            return new IoEither<TLeft, TResult>(_self.SelectMany(either => either.Match(
+                left: l => Io<IEither<TLeft, TResult>>.Apply(() => l.AsLeft<TLeft, TResult>()),
+                right: r => f(r).Out())));
+        }
+    }
+
     public static class IoEither
     {
-        //public static Io<IEither<TLeft, TRight>> AsLeftIo<TLeft, TRight>(this TLeft t)
-        //{
-        //    return Io<IEither<TLeft, TRight>>.Apply(() => t.AsLeft<TLeft, TRight>());
-        //}
+        public static IoEither<TLeft, TRight> In<TLeft, TRight>(this Io<IEither<TLeft, TRight>> io)
+        {
+            return new IoEither<TLeft, TRight>(io);    
+        }
 
-        //public static Io<IEither<TLeft, TRight>> AsRightIo<TLeft, TRight>(this TRight t)
-        //{
-        //    return Io<IEither<TLeft, TRight>>.Apply(() => t.AsRight<TLeft, TRight>());
-        //}
+        public static IoEither<TLeft, TRight> ToIoEither<TLeft, TRight>(this IEither<TLeft, TRight> either)
+        {
+            return new IoEither<TLeft, TRight>(either);
+        }
 
-        //public static Io<IEither<TLeft, TResult>> Select<TLeft, TRight, TResult>(this Io<IEither<TLeft, TRight>> ioT,
-        //    Func<TRight, TResult> f)
-        //{
-        //    return ioT.Select(e => e.Select(f));
-        //}
+        public static IoEither<TLeft, TRight> AsLeftIo<TLeft, TRight>(this TLeft t)
+        {
+            return new IoEither<TLeft, TRight>(t);
+        }
 
-        //public static Io<IEither<TLeft, TResult>> SelectMany<TLeft, TRight, TResult>(
-        //    this Io<IEither<TLeft, TRight>> ioT, Func<TRight, Io<IEither<TLeft, TResult>>> f)
-        //{
-        //    return ioT.SelectMany(e => e.Match(
-        //        left: AsLeftIo<TLeft, TResult>,
-        //        right: f));
-        //}
+        public static IoEither<TLeft, TRight> AsRightIo<TLeft, TRight>(this TRight t)
+        {
+            return new IoEither<TLeft, TRight>(t);
+        }
 
-        //public static Io<IEither<TLeft, TSelect>> SelectMany<TLeft, TRight, TResult, TSelect>(
-        //    this Io<IEither<TLeft, TRight>> ioT, Func<TRight, Io<IEither<TLeft, TResult>>> f,
-        //    Func<TRight, TResult, TSelect> selector)
-        //{
-        //    return ioT.SelectMany(a => f(a).SelectMany(b => selector(a, b).AsRightIo<TLeft, TSelect>()));
-        //}
+        public static IoEither<TLeft, TResult> Select<TLeft, TRight, TResult>(this IoEither<TLeft, TRight> ioT,
+            Func<TRight, TResult> f)
+        {
+            return ioT.FMap(f);
+        }
+
+        public static IoEither<TLeft, TResult> SelectMany<TLeft, TRight, TResult>(
+            this IoEither<TLeft, TRight> ioT, Func<TRight, IoEither<TLeft, TResult>> f)
+        {
+            return ioT.Bind(f);
+        }
+
+        public static IoEither<TLeft, TSelect> SelectMany<TLeft, TRight, TResult, TSelect>(
+            this IoEither<TLeft, TRight> ioT, Func<TRight, IoEither<TLeft, TResult>> f,
+            Func<TRight, TResult, TSelect> selector)
+        {
+            return ioT.SelectMany(a => f(a).SelectMany(b => selector(a, b).AsRightIo<TLeft, TSelect>()));
+        }
     }
 }
