2001dc93b015d7cbd94dd5025f0bf8dfc2be0b14
Clif Kimball <ckimball@korewireless.com>
2016-05-11 15:53:09 -0400

    Maybe Single Unit Tests

--- FunctionalProgramming.Tests/EnumerableExtensionsTests.cs
+++ FunctionalProgramming.Tests/EnumerableExtensionsTests.cs
@@ -1,12 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Cryptography.X509Certificates;
-using FunctionalProgramming.Basics;
+﻿using FunctionalProgramming.Basics;
 using FunctionalProgramming.Helpers;
 using FunctionalProgramming.Monad;
 using FunctionalProgramming.Monad.Transformer;
 using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
 
 namespace FunctionalProgramming.Tests
 {
@@ -18,7 +18,7 @@ namespace FunctionalProgramming.Tests
             return t.ToMaybe().Where(predicate);
         }
 
-        [TestCase(new []{5,6,7,8}, true, new[]{5,6,7,8})]
+        [TestCase(new[] { 5, 6, 7, 8 }, true, new[] { 5, 6, 7, 8 })]
         public void TestTraverseMaybe(int[] testData, bool isSome, int[] expected)
         {
             Func<int, bool> greaterThanFour = i => i > 4;
@@ -27,16 +27,16 @@ namespace FunctionalProgramming.Tests
             Assert.AreEqual(isSome, !result.IsEmpty);
             if (isSome)
             {
-                (result.Match(
+                result.Match(
                     just: actual => Io.Apply(() => Assert.AreEqual(expected, actual)),
-                    nothing: () => Io.Apply(() => Assert.Fail("Expected to get a result for input {0} but got nothing instead", testData)))).UnsafePerformIo();
+                    nothing: () => Io.Apply(() => Assert.Fail("Expected to get a result for input {0} but got nothing instead", testData))).UnsafePerformIo();
             }
         }
 
         [Test]
         public void TestTraverseIo()
         {
-            var xs = new[] {1, 2, 3, 4, 5};
+            var xs = new[] { 1, 2, 3, 4, 5 };
             var result = xs.Traverse(x => from _1 in Io.Apply(() => Console.WriteLine(x)) select x).UnsafePerformIo();
             Assert.AreEqual(xs, result);
         }
@@ -53,10 +53,78 @@ namespace FunctionalProgramming.Tests
         [Test]
         public void TestTraverseStateIo()
         {
-            var xs = new[] {1, 2, 3, 4, 5};
+            var xs = new[] { 1, 2, 3, 4, 5 };
 
             var result = xs.Select(Count).Sequence().RunIo(0).UnsafePerformIo();
-            Assert.AreEqual(5, result.Item1);           
+            Assert.AreEqual(5, result.Item1);
+        }
+
+        [TestCase]
+        public void MaybeSingleTest_EmptyEnumerableWithoutPredicateReturnsNothing()
+        {
+            var result = new List<TestUser>().MaybeSingle();
+            Assert.IsTrue(result.IsEmpty);
+        }
+
+        [TestCase]
+        public void MaybeSingleTest_EmptyEnumerableWithPredicateReturnsNothing()
+        {
+            var result = new List<TestUser>().MaybeSingle(x => x.Identity.Equals(new GenericIdentity("a")));
+            Assert.IsTrue(result.IsEmpty);
+        }
+
+        [TestCase]
+        public void MaybeSingleTest_MultipleEnumerableWithoutPredicateReturnsNothing()
+        {
+            var result = new List<TestUser> { new TestUser { Email = "a" }, new TestUser { Email = "a" } }.MaybeSingle();
+            Assert.IsTrue(result.IsEmpty);
+        }
+
+        [TestCase]
+        public void MaybeSingleTest_MultipleEnumerableWithPredicateReturnsNothing()
+        {
+            var result = new List<TestUser> { new TestUser { Email = "a" }, new TestUser { Email = "a" } }.MaybeSingle(x => x.Identity.Equals(new GenericIdentity("a")));
+            Assert.IsTrue(result.IsEmpty);
+        }
+
+        [TestCase]
+        public void MaybeSingleTest_EmptyQueryableWithoutPredicateReturnsNothing()
+        {
+            var result = new List<TestUser>().AsQueryable().MaybeSingle();
+            Assert.IsTrue(result.IsEmpty);
+        }
+
+        [TestCase]
+        public void MaybeSingleTest_EmptyQueryableWithPredicateReturnsNothing()
+        {
+            var result = new List<TestUser>().AsQueryable().MaybeSingle(x => x.Identity.Equals(new GenericIdentity("a")));
+            Assert.IsTrue(result.IsEmpty);
+        }
+
+        [TestCase]
+        public void MaybeSingleTest_MultipleQueryableWithoutPredicateReturnsNothing()
+        {
+            var result = new List<TestUser> { new TestUser { Email = "a" }, new TestUser { Email = "a" } }.AsQueryable().MaybeSingle();
+            Assert.IsTrue(result.IsEmpty);
+        }
+
+        [TestCase]
+        public void MaybeSingleTest_MultipleQueryableWithPredicateReturnsNothing()
+        {
+            var result = new List<TestUser> { new TestUser { Email = "a" }, new TestUser { Email = "a" } }.AsQueryable().MaybeSingle(x => x.Identity.Equals(new GenericIdentity("a")));
+            Assert.IsTrue(result.IsEmpty);
+        }
+
+        public class TestUser : IPrincipal
+        {
+            public string Email { get; set; }
+
+            public bool IsInRole(string role)
+            {
+                throw new NotImplementedException();
+            }
+
+            public IIdentity Identity => new GenericIdentity(Email);
         }
     }
 }
